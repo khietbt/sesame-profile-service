7ad2d07362f32eb46215067f89a3800bf93b0d65
khietbt <khietbt@gmail.com>
2023-11-07 10:49:13 +0000

    Use annotation to on/off security configurations

--- src/main/java/io/github/khietbt/SesameProfileService.java
+++ src/main/java/io/github/khietbt/SesameProfileService.java
@@ -1,14 +1,20 @@
 package io.github.khietbt;
 
+import io.github.khietbt.annotations.EnableSesameKeycloakSecurityConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @EnableR2dbcAuditing
 @Slf4j
 @SpringBootApplication
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@EnableSesameKeycloakSecurityConfiguration
 public class SesameProfileService implements CommandLineRunner {
 
     public static void main(String[] args) {
